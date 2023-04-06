24c9fffd62bd3eb78991a5f52a19a5222aa2aea8
Kyra Henningson <kyrahenningson@hotmail.com>
2023-04-06 15:45:57 -0700

    coding challenge attempt

--- src/look_here/Page.tsx
+++ src/look_here/Page.tsx
@@ -1,28 +1,43 @@
 import React from "react";
-import {starterTodos, TodoType} from "./index";
+import { starterTodos, TodoType } from "./index";
 import TodoList from "./TodoList";
 import SelectedTodo from "./SelectedTodo";
 
 const Page: React.FC = () => {
+  const [todos, setTodos] = React.useState<TodoType[]>(starterTodos);
 
-    const [todos, setTodos] = React.useState<TodoType[]>(starterTodos)
+  const handleSubmit = (newTodo: TodoType) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === newTodo.id) {
+          return newTodo;
+        }
+        return todo;
+      })
+    );
+  };
 
-    return (
-        <div style={{
-            display: "flex",
-            flexDirection: "row",
-            width: '100%',
-            height: '100%',
-            justifyContent: "space-around"
-        }}>
-            <div style={{display: 'flex', flexDirection: 'column'}}>
-                <SelectedTodo content={todos[0]}/>
-            </div>
-            <div style={{display: 'flex', flexDirection: 'column'}}>
-                <TodoList todos={todos}/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        height: "100%",
+        justifyContent: "space-around",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <SelectedTodo
+          content={todos[0]}
+          handleSubmit={(newTodo) => handleSubmit(newTodo)}
+        />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <TodoList todos={todos} />
+      </div>
+    </div>
+  );
+};
 
 export default Page;
--- src/look_here/SelectedTodo.tsx
+++ src/look_here/SelectedTodo.tsx
@@ -1,23 +1,68 @@
 import React from "react";
-import {TodoType} from "./index";
+import { TodoType } from "./index";
+import { useState, useCallback } from "react";
 
-type TodoProps = { content: TodoType };
-const SelectedTodo: React.FC<TodoProps> = ({content}) => {
+type TodoProps = {
+  content: TodoType;
+  handleSubmit: (editedTodo: TodoType) => void;
+};
+const SelectedTodo: React.FC<TodoProps> = ({ content, handleSubmit }) => {
+  const [title, setTitle] = React.useState<string | undefined>(undefined);
+  const [description, setDescription] = React.useState<string | undefined>(
+    undefined
+  );
 
+  const handleClick = React.useCallback(() => {
+    if (title !== undefined && description !== undefined) {
+      handleSubmit({
+        title: title,
+        description: description,
+        id: content.id,
+      });
+    }
+    console.log("No title or description");
+  }, []);
 
-    return (
-        <div style={{width: 200, height: 400, display: "flex", flexDirection: "column"}}>
-            <h2>Selected Todo</h2>
-            <label style={{display: "flex", flexDirection: "column"}}>Title
-                <input defaultValue={content.title}/>
-            </label>
-            <label style={{display: "flex", flexDirection: "column"}}>Description
-                <input defaultValue={content.description}/>
-            </label>
+  return (
+    <div
+      style={{
+        width: 200,
+        height: 400,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h2>Selected Todo</h2>
+      <label style={{ display: "flex", flexDirection: "column" }}>
+        Title
+        <input
+          onChange={(event) => {
+            console.log(event.target.value);
+            setTitle(event.target.value);
+          }}
+          defaultValue={content.title}
+        />
+      </label>
+      <label style={{ display: "flex", flexDirection: "column" }}>
+        Description
+        <input
+          onChange={(event) => {
+            console.log(event.target.value);
+            setDescription(event.target.value);
+          }}
+          defaultValue={content.description}
+        />
+      </label>
 
-            <button style={{marginTop: 5}}>Save Data</button>
-
-        </div>
-    )
-}
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+        style={{ marginTop: 5 }}
+      >
+        Save Data
+      </button>
+    </div>
+  );
+};
 export default SelectedTodo;
